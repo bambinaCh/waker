f3421f2864a56b22118603ba0a19586c0e15700f
bambinaCh <chamaa.jarite@gmail.com>
2025-03-19 15:29:43 +0100

    WakerMainScreen funktion und Prewie in eine neue Datei auslageret

--- app/src/main/java/uek/cj/waker/WakerMainScreen.kt
+++ app/src/main/java/uek/cj/waker/WakerMainScreen.kt
@@ -1,2 +1,67 @@
 package uek.cj.waker
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import uek.cj.waker.ui.theme.WakerTheme
+
+@Composable
+fun WakerMainScreen() {
+    var text by rememberSaveable { mutableStateOf("") } // speichert text eingabe
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "waker app lauft",
+            modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it},
+            label = { Text("schreib mal was") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("here to be press")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun WakerMainScreenPreview() {
+    WakerTheme {
+        WakerMainScreen()
+    }
+}
