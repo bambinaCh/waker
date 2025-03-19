3ec6ac14b53962fa44132de439190a9a26e0822f
bambinaCh <chamaa.jarite@gmail.com>
2025-03-19 14:39:53 +0100

    vertikale ansicht mit text, eingabefeld und button

--- app/src/main/java/uek/cj/waker/MainActivity.kt
+++ app/src/main/java/uek/cj/waker/MainActivity.kt
@@ -1,48 +1,85 @@
 package uek.cj.waker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import uek.cj.waker.ui.theme.WakerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            WakerTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-
-                    WakerMainScreen(
-                        name = "Waker", modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        enableEdgeToEdge() // vollbildmodus aktivieren
+        setContent { // legt ui fest
+            WakerTheme { // app design
+                WakerMainScreen()
             }
         }
     }
 }
 
 @Composable
-fun WakerMainScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name APP!", modifier = modifier
-    )
+fun WakerMainScreen() {
+    var text by rememberSaveable { mutableStateOf("") } // speichert eingabe
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "waker app lauft",
+            modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it},
+            label = { Text("schreib mal was") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("here to be press")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun WakerMainScreenPreview() {
     WakerTheme {
-        WakerMainScreen("Waker")
+        WakerMainScreen()
     }
 }
