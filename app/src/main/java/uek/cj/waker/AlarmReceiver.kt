8aaa7c8c1c85d29f48609748d1528bee4402be60
bambinaCh <chamaa.jarite@gmail.com>
2025-03-25 07:38:29 +0100

    Vibration in Alarm Receiver implementiert um diese beimm Auslösen der Alarm vibriert

--- app/src/main/java/uek/cj/waker/AlarmReceiver.kt
+++ app/src/main/java/uek/cj/waker/AlarmReceiver.kt
@@ -4,6 +4,9 @@ package uek.cj.waker
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.widget.Toast
 
 /* Quellen:
@@ -15,6 +18,14 @@ import android.widget.Toast
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) { // alarm wird automatisch ausgeloest
         Toast.makeText(context, "Waker!", Toast.LENGTH_LONG).show()
+
+        // Vibration beim auslösen der Waker
+        val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(10000, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(10000)
+        }
 
         val fullScreenIntent = Intent(context, AlarmActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
