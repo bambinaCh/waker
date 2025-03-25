199ca7e00f0497a5faa002df95195e9c2fc2e1d9
bambinaCh <chamaa.jarite@gmail.com>
2025-03-25 05:18:08 +0100

    Alarm activity grundgrüst

--- app/src/main/java/uek/cj/waker/AlarmActivity.kt
+++ app/src/main/java/uek/cj/waker/AlarmActivity.kt
@@ -1,2 +1,14 @@
 package uek.cj.waker
 
+import android.app.Activity
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
+
+class AlarmActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
